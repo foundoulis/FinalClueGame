f39a7fb2204505314f2a9d72880ec80e7514df4f
tylerdoll <tyler.r.doll@gmail.com>
2017-04-10 21:36:20 -0600

    Fixed My Cards to show only human player cards, not the whole deck

--- src/swinggui/MyCardsGUI.java
+++ src/swinggui/MyCardsGUI.java
@@ -1,11 +1,14 @@
 package swinggui;
 
+import java.util.ArrayList;
+
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 import player.HumanPlayer;
+import card.Card;
 import clueGame.Board;
 
 @SuppressWarnings("serial")
@@ -14,10 +17,24 @@ public class MyCardsGUI extends JPanel {
 	public MyCardsGUI(HumanPlayer humanPlayer) {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setBorder(new TitledBorder(new EtchedBorder(), "My Cards"));
-
-		Board board = Board.getInstance();
-		add(new CardPanel("Weapons", board.getWeaponCards()));
-		add(new CardPanel("People", board.getPeopleCards()));
-		add(new CardPanel("Rooms", board.getRoomCards()));
+		ArrayList<Card> weaponCards = new ArrayList<Card>();
+		ArrayList<Card> personCards = new ArrayList<Card>();
+		ArrayList<Card> roomCards = new ArrayList<Card>();
+		for (Card c : humanPlayer.getCards()) {
+			switch (c.getType()) {
+			case WEAPON:
+				weaponCards.add(c);
+				break;
+			case PERSON:
+				personCards.add(c);
+				break;
+			case ROOM:
+				roomCards.add(c);
+				break;
+			}
+		}
+		add(new CardPanel("Weapons", weaponCards));
+		add(new CardPanel("People", personCards));
+		add(new CardPanel("Rooms", roomCards));
 	}
 }
