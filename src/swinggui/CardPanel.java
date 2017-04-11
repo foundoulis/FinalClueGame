77ed829182a670c2cc4a1ad5b431a7ef25c570fe
tylerdoll <tyler.r.doll@gmail.com>
2017-04-10 19:44:09 -0600

    Added CardPanel to git

---
+++ src/swinggui/CardPanel.java
@@ -0,0 +1,25 @@
+package swinggui;
+
+import java.awt.LayoutManager;
+import java.util.List;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+import card.Card;
+
+@SuppressWarnings("serial")
+public class CardPanel extends JPanel {
+
+	public CardPanel(String title, List<Card> cards) {
+		setBorder(new TitledBorder(new EtchedBorder(), title));
+		for (Card c : cards) {
+			JTextField cardNameFld = new JTextField(20);
+			cardNameFld.setEditable(false);
+			cardNameFld.setText(c.getName());
+			add(cardNameFld);
+		}
+	}
+}
